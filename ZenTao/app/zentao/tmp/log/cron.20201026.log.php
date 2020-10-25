1877e43c8dc89193492fd5715a24582fc30a2989
Rudakov Daniil <dannikryd@mail.ru>
2020-10-25 19:42:08 +0300

    first commit

---
+++ ZenTao/app/zentao/tmp/log/cron.20201026.log.php
@@ -0,0 +1,29 @@
+<?php
+ die();
+0:40:05 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : 
+OK
+
+0:40:05 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : 
+NO WEBHOOK EXIST.
+
+0:40:05 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+
+0:40:05 task 12 executed,
+command: moduleName=ci&methodName=checkCompileStatus.
+return : .
+output : 
+success
+0:40:05 task 13 executed,
+command: moduleName=ci&methodName=exec.
+return : .
+output : 
+success
